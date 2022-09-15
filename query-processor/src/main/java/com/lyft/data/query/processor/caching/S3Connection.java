4dee43eb630a6b231d1c70370fa2fdee2da029c0
RohanCherivirala <Rohan.Cherivirala@6sense.com>
2022-09-15 18:46:20 -0400

    INFRA-1046 Add import statement for jenkins

--- query-processor/src/main/java/com/lyft/data/query/processor/caching/S3Connection.java
+++ query-processor/src/main/java/com/lyft/data/query/processor/caching/S3Connection.java
@@ -4,6 +4,7 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.lyft.data.query.processor.config.QueryProcessorConfiguration;
 
 import java.util.Arrays;
